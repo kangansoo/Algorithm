d97223018ceeae5951983ac313552c32056ec941
Ansoo Kang <ansoo971@gmail.com>
2024-02-19 11:29:37 +0900

    [Bronze III] Title: 대지, Time: 2080 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -7,7 +7,12 @@ for _ in range(n):
     x.append(a)
     y.append(b)
 
+x1 = max(x) - min(x)
+
+y1 = max(y) - min(y)
+
+    
 if n<2:
     print(0)
 else:
-    print((max(x)-min(x))*(max(y)-min(y)))
+    print(x1*y1)
