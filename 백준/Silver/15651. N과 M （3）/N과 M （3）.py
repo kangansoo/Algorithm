acd17c139800e3e47a49b5df5596ae0c08562b5b
Ansoo Kang <ansoo971@gmail.com>
2024-04-13 19:49:49 +0900

    [Silver III] Title: N과 M (3), Time: 1960 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+def backtracking():
+    if len(result) == m:
+        print(*result)
+        return
+    for i in range(1, n+1):
+        result.append(i)
+        backtracking()
+        result.pop()
+
+n, m = map(int, input().split())
+result=[]
+backtracking()
