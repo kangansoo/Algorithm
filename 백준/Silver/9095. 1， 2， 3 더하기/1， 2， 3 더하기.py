22ddfb489831bc3241f5395c5d39bbcc20883cca
Ansoo Kang <ansoo971@gmail.com>
2024-04-27 22:03:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def func(x):
+    if x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    elif x == 3:
+        return 4
+    else:
+        return func(x-1) + func(x-2) + func(x-3)
+
+n = int(input())
+for _ in range(n):
+    m = int(input())
+    print(func(m))
