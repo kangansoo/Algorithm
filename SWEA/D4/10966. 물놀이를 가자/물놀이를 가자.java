8dc40891167a7ac235724d3123b2c5fc5587554b
Ansoo Kang <ansoo971@gmail.com>
2024-11-08 00:20:25 +0900

    [D4] Title: 물놀이를 가자, Time: 1,514 ms, Memory: 131,728 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -6,62 +6,60 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-    static int N, M;
+    static int N, M, answer;
+    static int[][] deltas={{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     static char[][] map;
     static boolean[][] visited;
-    static int[][] deltas = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-    	StringBuilder sb = new StringBuilder();
+    static Queue<int[]> q;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        q = new LinkedList<>();
         int T = Integer.parseInt(br.readLine());
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int t=1; t<=T; t++){
+            st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             map = new char[N][M];
             visited = new boolean[N][M];
-
-            for (int i = 0; i < N; i++) {
+            answer=0;
+            for(int i=0; i<N; i++){
                 String str = br.readLine();
-                for (int j = 0; j < M; j++) {
-                    map[i][j] = str.charAt(j);
-                }
-            }
-
-            int totalDistance = 0;
-
-            Queue<int[]> q = new LinkedList<>();
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 'W') {
-                        q.offer(new int[] {i, j, 0});
-                        visited[i][j] = true;
+                for(int j=0; j<M; j++){
+                    map[i][j]=str.charAt(j);
+                    if(map[i][j]=='W'){
+                        q.offer(new int[]{i, j, 0});
+                        visited[i][j]=true;
                     }
                 }
             }
-
-            while (!q.isEmpty()) {
-                int[] tmp = q.poll();
-                int x = tmp[0], y = tmp[1], dist = tmp[2];
-
-                for (int[] delta : deltas) {
-                    int nx = x + delta[0];
-                    int ny = y + delta[1];
-
-                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]) {
-                        if (map[nx][ny] == 'L') {
-                            totalDistance += dist + 1;
-                        }
-                        visited[nx][ny] = true;
-                        q.offer(new int[] {nx, ny, dist + 1});
-                    }
-                }
-            }
-            sb.append("#").append(t).append(" ").append(totalDistance).append("\n");
+            bfs();
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
         }
+
         System.out.println(sb);
+    }
+
+    static void bfs(){
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int cnt = curr[2];
+            if(map[x][y]=='L'){
+                answer+=cnt;
+            }
+            for(int d=0; d<4; d++){
+                int nx = x+deltas[d][0];
+                int ny = y+deltas[d][1];
+                if(nx>=0 && nx<N && ny>=0 && ny<M && !visited[nx][ny]){
+                    visited[nx][ny]=true;
+                    q.offer(new int[]{nx, ny, cnt+1});
+                }
+            }
+
+        }
     }
 }
