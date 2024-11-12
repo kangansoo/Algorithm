cc74340e9a723b155b25b8757875fcf87a067acc
Ansoo Kang <ansoo971@gmail.com>
2024-11-12 17:13:24 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 264 ms, Memory: 22536 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -5,65 +5,65 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static final int INF = Integer.MAX_VALUE;
 	static int N;
-	static int[][] map, deltas= {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-	
+	static int[][] map, dist, deltas= {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+	static boolean[][] visited;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int idx=1;
-		
-		while(true) {
-			N = Integer.parseInt(br.readLine());
-			if(N==0) break;
-			map = new int[N][N];
-			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken()); 
-				}
-			}
-			sb.append("Problem").append(" ").append(idx++).append(": ").append(minDistance(0, 0, N-1, N-1)).append("\n");
-		}
-		System.out.println(sb);
+        StringTokenizer st;
+        int idx=1;
+        
+        while(true) {
+        	N = Integer.parseInt(br.readLine());
+        	if(N==0) break;
+        	map = new int[N][N];
+        	dist = new int[N][N];
+        	visited = new boolean[N][N];
+        	for(int i=0; i<N; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for(int j=0; j<N; j++) {
+        			map[i][j] = Integer.parseInt(st.nextToken()); 
+        		}
+        	}
+        	
+        	sb.append("Problem ").append(idx++).append(": ").append(setDist()).append("\n");
+        	
+        }
+        System.out.println(sb);
 	}
 
-	static int minDistance(int sr, int sc, int er, int ec) {
-		final int INF = Integer.MAX_VALUE;
-		boolean[][] visited = new boolean[N][N];
-		int[][] minThief = new int[N][N];
-		PriorityQueue<int[]> pQueue = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
-		
+	static int setDist() {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->Integer.compare(a[2], b[2]));
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				minThief[i][j]=INF;
+				dist[i][j]=INF;
 			}
 		}
-		
-		minThief[sr][sc] = map[sr][sc];
-		pQueue.offer(new int[] {sr, sc, minThief[sr][sc]});
-		
-		while(!pQueue.isEmpty()) {
-			int[] stopOver = pQueue.poll();
-			int r = stopOver[0];
-			int c = stopOver[1];
-			int money = stopOver[2];
-			
-			if(visited[r][c]) continue;
-			visited[r][c] = true;
-			if(r==er && c==ec) return money;
-			
+		dist[0][0]=map[0][0];
+		pq.add(new int[] {0, 0, dist[0][0]});
+		visited[0][0]=true;
+		while(!pq.isEmpty()) {
+			int[] curr = pq.poll();
+			int x = curr[0];
+			int y = curr[1];
+			int money = curr[2];
+			if(x==N-1 && y==N-1) {
+				return money;
+			}
 			for(int d=0; d<4; d++) {
-				int nr = r+deltas[d][0];
-				int nc = c+deltas[d][1];
-				if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc] && minThief[nr][nc]>money+map[nr][nc]) {
-					minThief[nr][nc] = money+map[nr][nc];
-					pQueue.offer(new int[] {nr, nc, minThief[nr][nc]});
+				int nx = x+deltas[d][0];
+				int ny = y+deltas[d][1];
+				if(nx>=0 && nx<N && ny>=0 && ny<N && !visited[nx][ny] && money+map[nx][ny]<dist[nx][ny]) {
+					dist[nx][ny]=money+map[nx][ny];
+					visited[nx][ny]=true;
+					pq.add(new int[] {nx, ny, dist[nx][ny]});
 				}
 			}
+			
 		}
+		
 		return -1;
 	}
 }
