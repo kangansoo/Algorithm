8a1c717cb7f7f1d7fb5c0830bfa78ea33b7aa1bb
Ansoo Kang <ansoo971@gmail.com>
2025-04-18 17:38:46 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 560 ms, Memory: 292748 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -5,30 +5,31 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
         Queue<Integer> q = new LinkedList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for(int i=1; i<=N; i++){
+            q.add(i);
+        }
 
         sb.append("<");
-        for(int i=1; i<=n; i++){
-            q.offer(i);
+
+        while(q.size()>1){
+            for(int i=1; i<K; i++){
+                q.add(q.poll());
+            }
+            sb.append(q.poll()).append(",").append(" ");
         }
 
-        while(!q.isEmpty()){
-            for(int i=0; i<k-1; i++){
-                q.offer(q.poll());
-            }
-            sb.append(q.poll());
-            if(!q.isEmpty()){
-                sb.append(", ");
-            }
-        }
-        sb.append(">");
+        sb.append(q.poll()).append(">");
+
         System.out.println(sb);
     }
 }
