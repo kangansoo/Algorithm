61b81d3bf56156ee5a6d1568445baf8075f8f5c1
Ansoo Kang <ansoo971@gmail.com>
2025-08-17 10:11:21 +0900

    [level 2] Title: 주식가격, Time: 17.06 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        
+        for(int i=0; i<len; i++) {
+            int curr = prices[i];
+            int cnt=0;
+            for(int j=i+1; j<len; j++) {
+                cnt++;
+                if(curr>prices[j]) break;
+            }
+            answer[i]=cnt;
+        }
+        
+        return answer;
+    }
+}
