752f4b8e6dd2c44d895662a0f50e06e50d520335
Ansoo Kang <ansoo971@gmail.com>
2025-09-20 00:31:37 +0900

    [level 3] Title: 숫자 게임, Time: 115.02 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int j=B.length-1;
+        for(int i=A.length-1; i>=0; i--) {
+            if(B[j]>A[i]) {
+                answer++;
+                j--;
+            }
+        }
+
+        return answer;
+    }
+}
