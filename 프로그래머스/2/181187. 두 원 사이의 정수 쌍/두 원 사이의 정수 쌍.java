48652af357f2b102c38ce0d85ca26ee8491113fb
Ansoo Kang <ansoo971@gmail.com>
2025-09-29 11:26:24 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 22.05 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        
+        for(int i=1; i<=r2; i++) {
+            long y1 = (int)Math.floor(Math.sqrt(1.0*r2*r2-1.0*i*i));
+            long y2 = (int)Math.ceil(Math.sqrt(1.0*r1*r1-1.0*i*i));
+            
+            answer += (y1-y2+1);
+        }
+        
+        return answer*4;
+    }
+}
