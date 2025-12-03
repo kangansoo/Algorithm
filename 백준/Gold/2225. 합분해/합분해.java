76669f22f16e86ba9382290a389b079bfdbb8ca6
Ansoo Kang <ansoo971@gmail.com>
2025-12-03 16:54:56 +0900

    [Gold V] Title: 합분해, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int MOD = 1000000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N+1][K+1];
+
+        for(int i=1; i<=N; i++) dp[i][1]=1;
+        for(int i=1;i<=K;i++) dp[1][i]=i;
+
+        for(int i=2; i<=N; i++) {
+            for(int j=2; j<=K; j++) {
+                dp[i][j] = (dp[i-1][j]+dp[i][j-1])%MOD;
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
