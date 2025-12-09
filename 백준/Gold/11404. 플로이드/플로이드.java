bde78270f3d222e6c25416d3426ad32c81bd36ea
Ansoo Kang <ansoo971@gmail.com>
2025-12-09 13:47:31 +0900

    [Gold IV] Title: 플로이드, Time: 420 ms, Memory: 44992 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,53 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[N+1][N+1];
+
+        for(int i=1; i<=N; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph[a][b]=Math.min(graph[a][b], c);
+        }
+
+        for(int k=1; k<=N; k++) {
+            for(int i=1; i<=N; i++) {
+                for(int j=1; j<=N; j++) {
+                    if (graph[i][k] != Integer.MAX_VALUE && graph[k][j] != Integer.MAX_VALUE) {
+                        graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                    }
+                }
+            }
+        }
+
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=N; j++) {
+                if (graph[i][j] == Integer.MAX_VALUE || i == j) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(graph[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
