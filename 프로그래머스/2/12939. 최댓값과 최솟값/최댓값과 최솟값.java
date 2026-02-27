b699dee51fcaa2c41e665814e0da7012550ea295
Ansoo Kang <ansoo971@gmail.com>
2026-02-27 16:30:36 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 18.33 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -7,25 +7,14 @@ class Solution {
         String[] arr = s.split(" ");
         
         for(String a:arr) {
-            // System.out.print(isNegative(a));
-            int tmp=0;
-            if(isNegative(a)) {
-                tmp = Integer.parseInt(a.substring(1)) *(-1);
-            } else {
-                tmp = Integer.parseInt(a);
-            }
+            int tmp = Integer.parseInt(a);
             
-            if(tmp<min) min=tmp;
-            if(tmp>max) max=tmp;
+            min = Math.min(tmp, min);
+            max = Math.max(tmp, max);
         }
         
         answer+=min+" "+max;
         
         return answer;
-    }
-    
-    public boolean isNegative(String str) {
-        if(str.charAt(0)!='-') return false;
-        return true;
     }
 }
