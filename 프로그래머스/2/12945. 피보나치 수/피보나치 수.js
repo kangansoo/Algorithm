2240183bcd25c113a071b781f07d718ebab86f92
Ansoo Kang <ansoo971@gmail.com>
2026-03-03 12:34:29 +0900

    [level 2] Title: 피보나치 수, Time: 3.40 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,15 @@
+const MOD = 1234567;
+
+function solution(n) {
+    var answer = 0;
+    const dp = new Array(n+1);
+    
+    dp[0]=0;
+    dp[1]=1;
+    
+    for(let i=2; i<=n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2])%MOD;
+    }
+    
+    return dp[n];
+}
